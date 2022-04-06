1bbee974138f1571b4524fd14f9b9766cc432f21
Bbican <95518981+Bbican@users.noreply.github.com>
2022-04-05 22:56:55 -0400

    first

--- src/test/java/pages/CommonPageElements.java
+++ src/test/java/pages/CommonPageElements.java
@@ -29,4 +29,41 @@ public class CommonPageElements {
 
     @FindBy(xpath = "//*[@id=\"login-page\"]/div/form/div[3]/button[2]/span")
     public  WebElement singInButton;
+
+    @FindBy(xpath = "//*[@id=\"entity-menu\"]/a/span")
+    public  WebElement items;
+
+    @FindBy(xpath = "//*[@id=\"entity-menu\"]/div/a[6]/span")
+    public  WebElement room;
+
+    @FindBy(xpath = "//*[@id=\"jh-create-entity\"]/span")
+    public WebElement creatARoomButton;
+
+    @FindBy(xpath = "//*[@id=\"room-roomNumber\"]")
+    public  WebElement roomNummberTexBox;
+
+    @FindBy(xpath = "//*[@id=\"room-roomType\"]/option[3]")
+    public  WebElement roomTypeTexBox;
+
+    @FindBy(xpath = "//*[@id=\"room-status\"]")
+    public WebElement status;
+
+    @FindBy (xpath = "//*[@id=\"room-price\"]")
+    public WebElement priceTexBox;
+
+    @FindBy(xpath = "//*[@id=\"room-description\"]")
+    public WebElement descriptionTextBox;
+
+    @FindBy(xpath = "//*[@id=\"save-entity\"]/span")
+    public WebElement saveButton;
+
+    @FindBy(xpath = "//*[@id=\"app-view-container\"]/div/div/div/div/div[1]/div/table/thead/tr/th[7]/span")
+    public WebElement CreatedDate;
+
+    @FindBy(xpath = "//*[@id=\"app-view-container\"]/div/div/div/div/div[1]/div/table/tbody/tr[1]/td[2]")
+    public WebElement actualroomNumber;
+
+
+
+
 }
--- src/test/java/step_definitions/UI_step_definitions/Room_Step_Definitions.java
+++ src/test/java/step_definitions/UI_step_definitions/Room_Step_Definitions.java
@@ -1,5 +1,6 @@
 package step_definitions.UI_step_definitions;
 
+import com.github.javafaker.Faker;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import pages.CommonPageElements;
@@ -10,7 +11,9 @@ import static utilities.Driver.waitAndClick;
 import static utilities.Driver.waitAndSendText;
 
 public class Room_Step_Definitions {
-
+    String roomNumber;
+    String actualRoomNumber;
+    Faker faker = new Faker();
     CommonPageElements commonPageElements = new CommonPageElements();
 
     @Given("admin navigates to Medunna web site")
@@ -29,22 +32,44 @@ public class Room_Step_Definitions {
     }
     @Given("admin clicks items dropdown and click Room")
     public void admin_clicks_items_dropdown_and_click_room() {
+        waitAndClick(commonPageElements.items);
+        waitAndClick(commonPageElements.room);
+
 
     }
     @Given("admin clicks create a new room button")
     public void admin_clicks_create_a_new_room_button() {
+        waitAndClick(commonPageElements.creatARoomButton);
+
 
     }
     @Given("admin enters room number {string}, price {string} and select the room type as SUITE")
-    public void admin_enters_room_number_price_and_select_the_room_type_as_suite(String string, String string2) {
+    public void admin_enters_room_number_price_and_select_the_room_type_as_suite(String room_number, String price) {
+
+        roomNumber = faker.number().digits(5);
+        System.out.println(roomNumber);
+        waitAndSendText(commonPageElements.roomNummberTexBox,roomNumber);
+        waitAndClick(commonPageElements.roomTypeTexBox);
+        waitAndClick(commonPageElements.status);
+        waitAndSendText(commonPageElements.priceTexBox,String.valueOf(faker.number().numberBetween(1000,2500)));
+        waitAndSendText(commonPageElements.descriptionTextBox,faker.address().fullAddress());
 
     }
     @Given("admin hit the save button")
     public void admin_hit_the_save_button() {
+        waitAndClick(commonPageElements.saveButton);
 
     }
     @Then("verify the room creation")
     public void verify_the_room_creation() {
+
+        waitAndClick(commonPageElements.CreatedDate);
+        actualRoomNumber = commonPageElements.actualroomNumber.getText();
+        System.out.println(actualRoomNumber);
+
+
+
+
 
     }
 
